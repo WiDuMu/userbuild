207b2d5eed211d00108521732a2282bfb76dbe90
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-02-25 14:13:06 -0600

    Update README, add types, update build script, update Bun compatibility

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # userbuild
 2nd attempt to build a simple bundler for userscript development. It now uses the bun bundler, and injects the metadata comments back into the built JavaScript. Write your `.user.ts` or `.user.js` files in the `src` directory, and it will create a useable script in the `out` directory.
 
-This allows you to use typescript, npm packages, and newer syntax, and get a viable userscript out of it.
+This allows you to use typescript, npm packages, and newer syntax, and get a viable Greasemonkey/Tampermonkey/Violentmonkey compatible userscript out of it.
+
+### To download
+You can either use this template to create a github repository, to modify it, or download the zip locally to get the template.
 
 ### To install dependencies:
-Older versions supported nodejs, with the transision to bun bundler bun is the supported runtime.
+Older versions supported nodejs, with the transision to bun bundler bun is the only supported runtime. As Bun is the bundler, it doesn't make sense to use other runtimes.
 
 #### bun
 ```bash
--- scripts/build.ts
+++ scripts/build.ts
@@ -1,16 +1,17 @@
-const entrypoints = [...new Bun.Glob("src/*user.ts").scanSync("."), ...new Bun.Glob("src/*user.js").scanSync(".")];
-
+const entrypoints = [
+	...new Bun.Glob("src/*user.ts").scanSync("."),
+	...new Bun.Glob("src/*user.js").scanSync("."),
+];
 
 const result = await Bun.build({
-   entryPoints: entrypoints,
-   bundle: true,
-   outdir: 'out',
-   splitting: false,
-   sourcemap: "none",
-   treeShaking: true,
-   target: 'browser', // This is different from esbuild for some reason
-   format: 'iife', // Use either 'esm' or 'iife' depending on how old your targets are.
-   banner: `// ==UserScript==
+	entrypoints: entrypoints,
+	outdir: "out",
+	splitting: false,
+	sourcemap: "none",
+	target: "browser", // This is different from esbuild for some reason
+	format: "iife", // Use either 'esm' or 'iife' depending on how old your targets are.
+	/** The banner is used to configure your userscript */
+	banner: `// ==UserScript==
  // @name        New script 
  // @namespace   Violentmonkey Scripts
  // @match       *://example.org/*
@@ -18,7 +19,7 @@ const result = await Bun.build({
  // @version     1.0
  // @author      -
  // ==/UserScript==
- `
- })
+ `,
+});
 
- console.log(result);
+console.log(result);
